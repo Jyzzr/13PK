76f35c07b932c5686ae024b659ba0cec3fa25973
a3367890 <glenn0047@gmail.com>
2024-03-31 13:03:49 +0800

    FIX:修正contactWeb，room api串接修正

--- assets/Script/Displayer.ts
+++ assets/Script/Displayer.ts
@@ -8,7 +8,13 @@ import {
   Node,
   NodeEventType,
 } from "cc";
-import { contactweb, LocationMsg, RoomMsg, CardMsg } from "./contactweb";
+import {
+  contactweb,
+  LocationMsg,
+  RoomMsg,
+  CardMsg,
+  RoomInfo,
+} from "./contactweb";
 const { ccclass, property } = _decorator;
 
 enum StateType {
@@ -37,24 +43,6 @@ export class Displayer extends Component {
     tooltip: "Room scene",
   })
   private Room1Scene: Node = null;
-
-  @property({
-    type: Node,
-    tooltip: "Room scene",
-  })
-  private Room2Scene: Node = null;
-
-  @property({
-    type: Node,
-    tooltip: "Room scene",
-  })
-  private Room3Scene: Node = null;
-
-  @property({
-    type: Node,
-    tooltip: "Room scene",
-  })
-  private Room4Scene: Node = null;
 
   @property({
     type: EditBox,
@@ -99,16 +87,15 @@ export class Displayer extends Component {
   private SetCardButton: Button = null;
 
   private roomMemberNumberList: number[] = [0, 0, 0, 0];
+  private roomInfoList: RoomInfo[] = [];
 
   onLoad() {
     this.Room1Scene.active = false;
-    this.Room2Scene.active = false;
-    this.Room3Scene.active = false;
-    this.Room4Scene.active = false;
     this.LoginScene.active = true;
     this.LobbyScene.active = false;
-    // 設定Client Callback
+    // 設定Client Callback                 //data
     contactweb.SetCallback("LocationMsg", (message: LocationMsg) => {
+      console.log(message);
       if (message.Location === StateType.LOGOUT) {
         //
       } else if (message.Location === StateType.LOBBY) {
@@ -121,7 +108,9 @@ export class Displayer extends Component {
     });
 
     contactweb.SetCallback("RoomMsg", (message: RoomMsg) => {
-      console.log(message);
+      this.roomInfoList = message.Rooms;
+      console.log(message.Type);
+      console.log(message.Rooms);
     });
     contactweb.SetCallback("CardMsg", (message: CardMsg) => {
       console.log(message);
@@ -134,7 +123,7 @@ export class Displayer extends Component {
       this.LoginScene.active = false;
       this.LobbyScene.active = true;
       this.Namedisplay.string = this.Name.string;
-      contactweb.SendMessage("join.lobby", this.Name);
+      contactweb.SendMessage("join.lobby", this.Namedisplay.string);
     });
 
     this.ExitLobbyButton.node.on(NodeEventType.MOUSE_DOWN, () => {
@@ -154,20 +143,20 @@ export class Displayer extends Component {
         }
         this.LobbyScene.active = false;
         this.Room1Scene.active = true;
-        contactweb.SendMessage("join.room", "Room" + index);
+        contactweb.SendMessage("join.room", "Gay's Room" + index);
       });
     });
 
-    this.ExitRoomButton.node.on(NodeEventType.MOUSE_DOWN, () => {
-      contactweb.SendMessage("exit.room", "");
-    });
+    // this.ExitRoomButton.node.on(NodeEventType.MOUSE_DOWN, () => {
+    //   contactweb.SendMessage("exit.room", "");
+    // });
 
-    this.SetCardButton.node.on(NodeEventType.MOUSE_DOWN, () => {
-      contactweb.SendMessage(
-        "set.card",
-        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-      );
-    });
+    // this.SetCardButton.node.on(NodeEventType.MOUSE_DOWN, () => {
+    //   contactweb.SendMessage(
+    //     "set.card",
+    //     [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+    //   );
+    // });
   }
 
   // 如果有連上server，將GameState設為active(可開始遊戲)
--- assets/Script/contactweb.ts
+++ assets/Script/contactweb.ts
@@ -19,85 +19,90 @@ export interface LocationMsg {
 
 export interface RoomMsg {
   Type: string; // Response type
-  Location: number; // Player's location, 0: Logout, 1: Lobby, 2: Room
-  Name: string; // Location name
+  Rooms: RoomInfo[]; //All Rooms info
+}
+
+export interface RoomInfo {
+  Name: string; // Room's name
+  Players: string[]; // String array of length 4, the name of the players in this room, empty means space
 }
 
 export interface CardMsg {
   Type: string; // Response type
-  Location: number; // Player's location, 0: Logout, 1: Lobby, 2: Room
-  Name: string; // Location name
+  Cards: number[][]; // Player's location, 0: Logout, 1: Lobby, 2: Room
 }
 
-@ccclass("contactweb")
-export class contactweb extends Component {
+export class contactweb {
+  // contactweb private variable
   static ws: WebSocket;
   static uri: string = "ws://localhost";
   static callbackList: CallbackFunc[] = [];
   static messageList: Message[] = [];
-  private socket: WebSocket = null;
 
-  // web public variable
+  // contactweb public variable
   static IsOpen: boolean = false;
   static OnConnect(): void {}
   static OnDisconnect(): void {}
 
-  public static SetCallback(type: string, callback: any) {
+  //
+  public static SetCallback(name: string, callback: any) {
     for (let i = 0; i < contactweb.callbackList.length; i++) {
-      if (contactweb.callbackList[i].Type == type) {
+      if (contactweb.callbackList[i].Type == name) {
         contactweb.callbackList[i].callback = callback;
         console.log(this.callbackList);
         return;
       }
     }
-    contactweb.callbackList.push({ Type: type, callback });
+    contactweb.callbackList.push({ Type: name, callback });
   }
 
   public static Connect(uri: string) {
     // Create socket
     contactweb.uri = "ws://" + uri;
     contactweb.ws = new WebSocket(contactweb.uri);
+
+    //
+    contactweb.ws.onopen = async function () {
+      contactweb.IsOpen = true;
+      contactweb.messageList = [];
+      contactweb.OnConnect();
+      console.log("connected to " + contactweb.uri);
+
+      //
+      while (contactweb.IsOpen) {
+        if (contactweb.messageList.length > 0) {
+          let message = contactweb.messageList[0];
+          contactweb.messageList.shift();
+          for (let i = 0; i < contactweb.callbackList.length; i++) {
+            if (contactweb.callbackList[i].Type == message.Type) {
+              //console.log("await callback: " + message.Path);
+              await contactweb.callbackList[i].callback(message);
+            }
+          }
+        } else {
+          await sleep(10);
+        }
+      }
+    };
+
+    contactweb.ws.onclose = function (event) {
+      contactweb.IsOpen = false;
+      contactweb.messageList = [];
+      contactweb.OnDisconnect();
+      console.log("connection closed (" + event.code + ")");
+    };
+
+    contactweb.ws.onmessage = async function (event) {
+      let data = JSON.parse(event.data);
+      contactweb.messageList.push(data);
+    };
   }
 
-  protected onLoad(): void {
-    this.socket = new WebSocket("ws://13poker.garbagedee.edu.kg:2345");
-    this.socket.onopen = this.onOpen.bind(this);
-    this.socket.onmessage = this.onMessage.bind(this);
-    this.socket.onerror = this.onError.bind(this);
-    this.socket.onclose = this.onClose.bind(this);
+  public static Close() {
+    contactweb.ws.close();
   }
 
-  private onOpen(event: Event): void {
-    console.log("WebSocket connection established");
-    // 连接成功后的逻辑
-    // 可以在这里发送消息给服务器
-    // this.socket.send('Hello, server!');
-  }
-
-  private onMessage(event: MessageEvent): void {
-    console.log("Message from server:", event.data);
-    // 收到服务器发送的消息后的逻辑
-  }
-
-  private onError(event: Event): void {
-    console.error("WebSocket error:", event);
-    // 连接发生错误后的处理逻辑
-  }
-
-  private onClose(event: CloseEvent): void {
-    console.log("WebSocket connection closed");
-    // 连接关闭后的逻辑
-  }
-
-  public send(message: string): void {
-    if (this.socket.readyState === WebSocket.OPEN) {
-      this.socket.send(message);
-    } else {
-      console.warn("WebSocket is not open. Message not sent:", message);
-    }
-  }
-  /////待檢查
-  public static SendMessage(mType: string, data: any) {
+  public static SendMessage(mType: string, data?: any) {
     if (contactweb.IsOpen) {
       if (mType == "join.lobby") {
         contactweb.ws.send(JSON.stringify({ Type: mType, Name: data }));
@@ -113,8 +118,8 @@ export class contactweb extends Component {
       }
     }
   }
+}
 
-  public close(): void {
-    this.socket.close();
-  }
+async function sleep(ms) {
+  return new Promise((r) => setTimeout(r, ms));
 }
